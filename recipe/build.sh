24a07ce70c0678537fa6051a6daa5af80045c053
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-06-07 19:57:52 -0700

    Tune versions and build recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+set -e
+
 mkdir build
 cd build
 
-echo prefix is $PREFIX
+# Fix for missing liblzma.
+perl -pi -e "s#(/[^\s]*?lib)/lib([^\s]+).la#-L\$1 -l\$2#g" ${PREFIX}/lib/*.la
 
-# Only GCC 5 is supported for now. May need to have a check here.
+echo "PREFIX=${PREFIX}"
 
-GCC_DIR=$(dirname $(dirname $(which gcc)))
-cmake ..                                  \
-    -DCMAKE_CXX_FLAGS="-g -O3 -B$GCC_DIR" \
-    -DBoost_INCLUDE_DIR=$PREFIX/include   \
-    -DISIS_DEPS_DIR=$PREFIX               \
-    -DBINARYBUILDER_INSTALL_DIR=$PREFIX   \
-    -DCMAKE_VERBOSE_MAKEFILE=ON           \
-    -DCMAKE_C_COMPILER=$GCC_DIR/bin/gcc   \
-    -DCMAKE_CXX_COMPILER=$GCC_DIR/bin/g++
+cmake ..                                        \
+    -DCMAKE_BUILD_TYPE=Release                  \
+    -DCMAKE_PREFIX_PATH=${PREFIX}               \
+    -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}       \
+    -DCMAKE_CXX_FLAGS="-g -O3"                  \
+    -DBoost_INCLUDE_DIR=${BUILD_PREFIX}/include \
+    -DISIS_DEPS_DIR=${BUILD_PREFIX}             \
+    -DBINARYBUILDER_INSTALL_DIR=${BUILD_PREFIX} \
+    -DCMAKE_VERBOSE_MAKEFILE=ON                 \
     # -DVW_ENABLE_SSE=0 #  on pfe
 
 make -j${CPU_COUNT}
