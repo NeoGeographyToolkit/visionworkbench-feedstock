03ab0aaf5c10f0ce3d26c3ec583b9a9d9643bffa
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-06-26 16:44:17 -0700

    Minor formatting

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,16 +23,14 @@ if [ "$(uname)" = "Darwin" ]; then
         -DBINARYBUILDER_INSTALL_DIR=${PREFIX}       \
         -DCMAKE_VERBOSE_MAKEFILE=ON                 \
         -DCMAKE_C_COMPILER=/usr/bin/clang           \
-        -DCMAKE_CXX_COMPILER=/usr/bin/clang++       \
-        # -DVW_ENABLE_SSE=0 #  on pfe
+        -DCMAKE_CXX_COMPILER=/usr/bin/clang++
 else
     cmake ..                                        \
     -DCMAKE_PREFIX_PATH=${PREFIX}                   \
         -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}       \
         -DISIS_DEPS_DIR=${PREFIX}                   \
         -DBINARYBUILDER_INSTALL_DIR=${PREFIX}       \
-        -DCMAKE_VERBOSE_MAKEFILE=ON                 \
-        # -DVW_ENABLE_SSE=0 #  on pfe
+        -DCMAKE_VERBOSE_MAKEFILE=ON
 fi
 
 make -j${CPU_COUNT}
