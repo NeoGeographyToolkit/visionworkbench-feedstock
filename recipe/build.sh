5d5f826790aec371b599dc2f108a8d412e6248da
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-06-26 11:37:47 -0700

    Tested recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,13 +11,25 @@ cd build
 # Fix for missing liblzma.
 perl -pi -e "s#(/[^\s]*?lib)/lib([^\s]+).la#-L\$1 -l\$2#g" ${PREFIX}/lib/*.la
 
-cmake ..                                        \
+if [ "$(uname)" = "Darwin" ]; then
+    cmake ..                                        \
+        -DCMAKE_PREFIX_PATH=${PREFIX}               \
+        -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}       \
+        -DISIS_DEPS_DIR=${PREFIX}                   \
+        -DBINARYBUILDER_INSTALL_DIR=${PREFIX}       \
+        -DCMAKE_VERBOSE_MAKEFILE=ON                 \
+        -DCMAKE_C_COMPILER=/usr/bin/clang           \
+        -DCMAKE_CXX_COMPILER=/usr/bin/clang++       \
+        # -DVW_ENABLE_SSE=0 #  on pfe
+else
+    cmake ..                                        \
     -DCMAKE_PREFIX_PATH=${PREFIX}               \
-    -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}       \
-    -DISIS_DEPS_DIR=${PREFIX}                   \
-    -DBINARYBUILDER_INSTALL_DIR=${PREFIX}       \
-    -DCMAKE_VERBOSE_MAKEFILE=ON                 \
-    # -DVW_ENABLE_SSE=0 #  on pfe
+        -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}       \
+        -DISIS_DEPS_DIR=${PREFIX}                   \
+        -DBINARYBUILDER_INSTALL_DIR=${PREFIX}       \
+        -DCMAKE_VERBOSE_MAKEFILE=ON                 \
+        # -DVW_ENABLE_SSE=0 #  on pfe
+fi
 
 make -j${CPU_COUNT}
 make install
