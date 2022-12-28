207fe776fea6208e349bb20266fc1912975205a9
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-27 20:05:18 -0800

    Update for ASP 3.2.0 release

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,30 +8,14 @@ export LDFLAGS="-L$PREFIX/lib"
 mkdir build
 cd build
 
-# Fix for missing liblzma.
-perl -pi -e "s#(/[^\s]*?lib)/lib([^\s]+).la#-L\$1 -l\$2#g" ${PREFIX}/lib/*.la
+# Recommended in https://gitter.im/conda-forge/conda-forge.github.io?at=5c40da7f95e17b45256960ce
+find ${PREFIX}/lib -name '*.la' -delete
 
-# On the Mac use the local compiler.
-# TODO(oalexan1): Figure out while the Mac conda
-# compiler results in stereo_pprc hanging when
-# doing threaded code.
-if [ "$(uname)" = "Darwin" ]; then
-    cmake ..                                        \
-        -DCMAKE_PREFIX_PATH=${PREFIX}               \
-        -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}       \
-        -DASP_DEPS_DIR=${PREFIX}                    \
-        -DBINARYBUILDER_INSTALL_DIR=${PREFIX}       \
-        -DCMAKE_VERBOSE_MAKEFILE=ON                 \
-        -DCMAKE_C_COMPILER=/usr/bin/clang           \
-        -DCMAKE_CXX_COMPILER=/usr/bin/clang++
-else
-    cmake ..                                        \
-    -DCMAKE_PREFIX_PATH=${PREFIX}                   \
-        -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}       \
-        -DASP_DEPS_DIR=${PREFIX}                    \
-        -DBINARYBUILDER_INSTALL_DIR=${PREFIX}       \
-        -DCMAKE_VERBOSE_MAKEFILE=ON
-fi
+cmake ..                             \
+    -DCMAKE_PREFIX_PATH=${PREFIX}    \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DASP_DEPS_DIR=${PREFIX}         \
+    -DCMAKE_VERBOSE_MAKEFILE=ON
 
 make -j${CPU_COUNT}
 make install
