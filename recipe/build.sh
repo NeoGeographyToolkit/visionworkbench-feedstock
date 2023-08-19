41d3d1e3b95b5c49158babe937bf6808db205bfd
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-08-18 17:30:18 -0700

    Update build for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,19 @@ cd build
 # Recommended in https://gitter.im/conda-forge/conda-forge.github.io?at=5c40da7f95e17b45256960ce
 find ${PREFIX}/lib -name '*.la' -delete
 
+isMac=$(uname -s | grep Darwin)
+if [ "$isMac" != "" ]; then
+  # Having trouble with the conda compiler on the mac for ipfind and convert_pinhole_model  
+  #opt="-DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++"
+  #echo old CXXFLAGS=$CXXFLAGS
+  export CXXFLAGS=""
+  opt=""
+else
+  opt=""
+fi
+
 cmake ..                             \
+    $opt                             \
     -DCMAKE_PREFIX_PATH=${PREFIX}    \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DASP_DEPS_DIR=${PREFIX}         \
